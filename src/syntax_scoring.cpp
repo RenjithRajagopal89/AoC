6953f7f24385098409b64866749d4af70f66f6e4
Renjith Rajagopal <renjith.rajagopal@volvocars.com>
2022-02-15 09:41:16 +0100

    Modification includes following
     - Handle error when file opening failed
     - Avoid magic numbers
     - Added debug logs which can be enabled or disabled on compile time

--- src/syntax_scoring.cpp
+++ src/syntax_scoring.cpp
@@ -3,22 +3,37 @@
 #include <vector>
 #include <map>
 #include <stack>
+#include <stdexcept>
 
 using namespace std;
 
+// #define DEBUG_EN
+
+#ifdef DEBUG_EN
+#define DEBUG_MSG(str) do { std::cout << str << std::endl; } while( false )
+#else
+#define DEBUG_MSG(str) do { } while ( false )
+#endif
+
 namespace navigation {
+
+    static const std::uint64_t CLOSING_PARANTHESIS_SCORE_VALUE = 3;
+    static const std::uint64_t CLOSING_SQUARE_BRACKET_SCORE_VALUE = 57;
+    static const std::uint64_t CLOSING_CURLY_BRACKET_SCORE_VALUE = 1197;
+    static const std::uint64_t CLOSING_ANGLE_BRACKET_SCORE_VALUE = 25137;
+
 
     std::vector<std::string> GetSyntaxChuckVector(const std::string& input_file) {
         std::ifstream infile(input_file);
         std::vector<std::string> lines;
         if (!infile.is_open()) {
             // Handle Error
+            throw std::runtime_error("Input file failed to open !");
         }
 
         std::string line;
         while (std::getline(infile, line)) {
             lines.push_back(line);
-            //cout << "Line is :" << line <<"\n";
         }
         infile.close();
         return lines;
@@ -49,16 +64,19 @@ namespace navigation {
 
         for (const auto &line_by_line : lines) {
             for(auto &character : line_by_line) {
-                cout << "Character is :" << character << "\n";
+                DEBUG_MSG("Character is : " << character << "\n" );
                 auto item = open_close_characters.find(character);
                 if (item != open_close_characters.end()) { // Closing character found
+                    DEBUG_MSG("Closing character found" << "\n" );
                     // Check top in stack and pop accordingly
                     if (!syntax_stack.empty()) {
                         //char top_character = ;
                         if (syntax_stack.top() == item->second) {
+                            DEBUG_MSG("Matching stack element.Removing from stack" << "\n" );
                             // Pair is OK. So Pop element
                             syntax_stack.pop();
                         } else {
+                            DEBUG_MSG("Mismatch stack element!" << "\n" );
                             auto elem = syntax_error_count.find(item->first);
                             if (elem != syntax_error_count.end()) {
                                 elem->second = elem->second + 1;
@@ -66,9 +84,8 @@ namespace navigation {
                             break;
                         }
                     }
-
                 } else { // Opening character found so Push map value to stack
-                    cout << "Opening char found:" << character << ' ' <<"Adding it to stack" << "\n";
+                    DEBUG_MSG("Opening character found:" << character << ' ' <<"Adding it to stack" << "\n");
                     syntax_stack.push(character);
                 }
             }
@@ -76,26 +93,27 @@ namespace navigation {
 
         std::uint64_t total_syntax_score = 0;
         for(const auto& elem : syntax_error_count) {
+            DEBUG_MSG("Syntax score map data: " << elem.first << ' ' << elem.second << "\n");
             switch (elem.first)
 	        {
 	            case ')':
-		            total_syntax_score += elem.second * 3; break;
+		            total_syntax_score += elem.second * CLOSING_PARANTHESIS_SCORE_VALUE; break;
 
 	            case '>':
-		            total_syntax_score += elem.second * 25137; break;
+		            total_syntax_score += elem.second * CLOSING_ANGLE_BRACKET_SCORE_VALUE; break;
 
 	            case ']':
-		            total_syntax_score += elem.second * 57; break;
+		            total_syntax_score += elem.second * CLOSING_SQUARE_BRACKET_SCORE_VALUE; break;
 
                 case '}':
-		            total_syntax_score += elem.second * 1197; break;
+		            total_syntax_score += elem.second * CLOSING_CURLY_BRACKET_SCORE_VALUE; break;
 
 	            default:
-		            cout<<"Unknown character"; break;
+		            std::cerr << "Unknown value" <<"\n"; break;
 
 	        }
         }
-    cout << "Total syntax score : " << total_syntax_score <<"\n";
+
     return total_syntax_score;
 }
 } // namespace navigation
@@ -106,6 +124,7 @@ int main(int argc, char* argv[]) {
     if (argc == 2) {
         const std::string navi_file_path = argv[1];
         auto syntax_score = navigation::RetriveSyntaxScore(navi_file_path);
+        cout << "Total syntax score : " << syntax_score <<"\n";
     } else {
         std::cerr << "Usage: " << "syntax_scoring <input file path>" << std::endl;
     }
